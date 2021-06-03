958201dcb86607dcd34e8d9246a2cdae0aef6736
codeberkay <50360041+codeberkay@users.noreply.github.com>
2021-06-03 14:47:52 +0300

    Updates

---
+++ static/js/main.ac630ae9.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-react-website"]=this["webpackJsonpmy-react-website"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);n(1);var c=n(3),r=n.n(c),s=(n(8),n.p,n(9),n(0));var i=function(){return Object(s.jsx)("h1",{children:"deneme"})};r.a.render(Object(s.jsx)(i,{}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.ac630ae9.chunk.js.map
